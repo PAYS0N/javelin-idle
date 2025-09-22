415cbf752e415ffe76c99dd711079221c9ba209e
pays0n <63822744+PAYS0N@users.noreply.github.com>
2025-09-22 12:27:48 -0500

    Display and print symbols instead of codes

--- js/javascript.js
+++ js/javascript.js
@@ -11,8 +11,24 @@ function setConstants() {
     userInput = document.querySelector(".typing-input")
     goalDisplay = document.querySelector(".goal-display")
     purchaseChar = "$"
-    charactersToType = ["{", "}", "(", ")", "*", "+", "=", ",", ".", "[", "]"]
-    if (charactersToType.includes(purchaseChar)) {console.error("chars must not contain purchase char")}
+    keyCodesToSymbols = {
+        "{": "{",
+        "}": "}",
+        "(": "(",
+        ")": ")",
+        "*": "*",
+        "+": "+",
+        "=": "=",
+        ",": ",",
+        ".": ".",
+        "[": "[",
+        "]": "]",
+        "ArrowUp": "↑",
+        "ArrowDown": "↓",
+        "ArrowLeft": "←",
+        "ArrowRight": "→"
+    }
+    if ( Object.keys(keyCodesToSymbols).includes(purchaseChar)) {console.error("chars must not contain purchase char")}
     upgrades = []    
 }
 
@@ -23,36 +39,47 @@ function doPageSetup() {
 }
 
 function updateScore() {
-    scoreDisplay.textContent = "Score: " + score
+    scoreDisplay.textContent = "Characters typed: " + score
 }
 
 function doGameSetup() {
     runGameLogic()
+    userInput.addEventListener("keydown", (e) => {verifyInput(e)})
     let autoTyper = new Upgrade("Auto-typer", 20, generateKey(3), 3, .25, document.querySelector(".first-upgrade"))
     upgrades.push(autoTyper)
 }
 
-function getInput() {
-    return userInput.value
+function getInput(e) {
+    return userInput.value + keyCodesToSymbols[e.key]
 }
 
 function runGameLogic() {
     setInterval(() => {
-        input = getInput()
-        if (isInputScorable(input)) {
-            inputCorrect()
-        }
-        else {
-            upgrade = returnUpgradeByKey(input)
-            if (upgrade) {
-                purchaseUpgrade(upgrade)
-            }
-        }
         revealUpgrades()
         displayUpgrades()
         updateScore()
     }, 100)
     setInterval(addAutoScore, 1000)
+}
+
+function verifyInput(e) {
+    let input = getInput(e)
+    if (isInputScorable(input)) {
+        e.preventDefault()
+        return inputCorrect()
+    }
+    else {
+        upgrade = returnUpgradeByKey(input)
+        if (upgrade) {
+            e.preventDefault()
+            return purchaseUpgrade(upgrade)
+        }
+    }
+    if(["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.key)){
+        e.preventDefault()
+        userInput.value += keyCodesToSymbols[e.key]
+    }
+
 }
 
 function addAutoScore() {
@@ -69,14 +96,15 @@ function inputCorrect() {
 }
 
 function updateGoal() {
-    let nextIndex = Math.floor(Math.random() * charactersToType.length)
-    let charToType = charactersToType[nextIndex]
-    goal = charToType
-    displayGoal()
+    let keyCodes = Object.keys(keyCodesToSymbols)
+    let nextIndex = Math.floor(Math.random() * keyCodes.length)
+    let symbolToType = keyCodesToSymbols[keyCodes[nextIndex]]
+    goal = symbolToType
+    displayGoal(symbolToType)
 }
 
-function displayGoal() {
-    goalDisplay.textContent = "Type: " + goal
+function displayGoal(symbol) {
+    goalDisplay.textContent = "Type: " + symbol
 }
 
 function isInputScorable(input) {
@@ -106,10 +134,11 @@ function returnUpgradeByKey(input) {
 }
 
 function generateKey( number) {
+    let keyCodes = Object.keys(keyCodesToSymbols)
     let aChars = [ purchaseChar]
     for(let i=0; i< number; i++){
-        let nextIndex = Math.floor(Math.random() * charactersToType.length)
-        aChars.push(charactersToType[nextIndex])
+        let nextIndex = Math.floor(Math.random() * keyCodes.length)
+        aChars.push(keyCodesToSymbols[keyCodes[nextIndex]])
     }
     return aChars.join("")
 }
